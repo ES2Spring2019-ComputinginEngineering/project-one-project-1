5b5c96105ce54a1a81c6340a03e6975ddacb9136
Megan Jenney <megan.jenney@tufts.edu>
2019-03-04 15:29:11 -0500

    Create simulation graphs.py

---
+++ simulation graphs.py
@@ -0,0 +1,64 @@
+import math
+import matplotlib.pyplot as plt
+import numpy
+
+g = -9.81
+L = 1
+dt = .0001
+
+run_time = 20
+init_ang_pos = (math.pi)/6
+init_ang_vel = 0
+init_ang_accel = (g/L) * math.sin(init_ang_pos)
+
+# lists that will be appended
+list_of_times = [0]
+ang_pos = [init_ang_pos]
+ang_vel = [init_ang_vel]
+ang_accel = [init_ang_accel]
+
+# def pendulum_values(run_time, init_ang_pos, init_ang_vel):
+i = 1
+while i <= (run_time / dt):
+    time = list_of_times[i-1] + dt
+    list_of_times.append(time)
+    new_ang_pos = ang_pos[i-1] + dt * ang_vel[i-1]
+    ang_pos.append(new_ang_pos)
+    new_ang_vel = ang_vel[i-1] + dt * ang_accel[i-1]
+    ang_vel.append(new_ang_vel)
+    new_ang_accel = (g/L) * math.sin(new_ang_pos)
+    ang_accel.append(new_ang_accel)
+    # print("Time" + str(i) + ", Position = " + str(ang_pos[i]) + ", Velocity = " + str(ang_vel[i]) + ", Acceleration = " + str(ang_accel[i]))
+    i += 1
+    # return "pendulum calculation finished at " + str(time) + " seconds"
+
+# print(pendulum_values(run_time, init_ang_pos, init_ang_vel))
+plt.figure(figsize = (12, 12))    
+plt.subplot(5, 1, 1)
+plt.plot(list_of_times, ang_pos, 'r-')
+
+
+plt.xlabel("Time (seconds)")
+plt.ylabel("Angular Position (radians)")
+plt.title("Angular Position vs. Time")
+plt.xlim(0, 20)
+plt.grid()
+
+
+plt.subplot(5, 1, 3)
+plt.plot(list_of_times, ang_vel, 'b-')
+
+plt.xlabel("Time (seconds)")
+plt.ylabel("Angular Velocity (radians/second)")
+plt.title("Angular Velocity vs. Time")
+plt.xlim(0, 20)
+plt.grid()
+
+plt.subplot(5, 1, 5)
+plt.plot(list_of_times, ang_accel, 'g-')
+
+plt.xlabel("Time (seconds)")
+plt.ylabel("Angular Acceleration (radians/second/second)")
+plt.title("Angular Acceleration vs. Time")
+plt.xlim(0, 20)
+plt.grid()
