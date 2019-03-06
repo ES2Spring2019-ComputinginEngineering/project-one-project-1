84e6e4f9d8516613d877e274ddce8c13063d673a
awong15 <Alycia.Wong@tufts.edu>
2019-03-06 14:56:03 -0500

    With example csv files

---
+++ Check.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  6 14:46:13 2019
+
+@author: alyci
+"""
+
+import Simulation as sim
+
+array = sim.read_file("Position", 0)
+
+print(array[:10])
--- simulation graphs.py
+++ simulation graphs.py
@@ -21,16 +21,15 @@ sim.plot_pva(list_of_times, ang_pos, ang_vel, ang_accel, run_time)
 
 m = input("What run trial would you like to use? ")
 
-pos = sim.read_file("Position", 0)
-vel = sim.read_file("Velocity", 0)
-accel = sim.read_file("Acceleration", 0)
-t = sim.read_file("Time", 0)
+pos = sim.read_file("Position", m)
+vel = sim.read_file("Velocity", m)
+accel = sim.read_file("Acceleration", m)
+t = sim.read_file("Time", m)
 
-"""ang_pos_filt = sim.apply_filt(ang_pos)
-ang_pos_peaks = sim.find_peaks(ang_pos_filt)
+ang_pos_filt = sim.apply_filter(ang_pos)
+# ang_pos_peaks = sim.find_peaks(ang_pos_filt)
 pos_filt = sim.apply_filter(pos)
-pos_peaks = sim.find_peaks(pos)
-pos_filt_peaks = sim.find_peaks(pos_filt)
-sim.plot_filtered(list_of_times, ang_pos, pos, ang_pos_filt, pos_filt,
+# pos_peaks = sim.find_peaks(pos)
+# pos_filt_peaks = sim.find_peaks(pos_filt)
+"""sim.plot_filtered(list_of_times, ang_pos, pos, ang_pos_filt, pos_filt,
                   ang_pos_peaks, pos_peaks, ang_pos_filt_peaks, ang_pos_peaks)"""
-
