bc75a47923226fc0747b99a588e41860f8ebf046
awong15 <Alycia.Wong@tufts.edu>
2019-03-07 17:10:53 -0500

    NO M

--- simulation graphs.py
+++ simulation graphs.py
@@ -18,24 +18,3 @@ ang_accel = [init_ang_accel]
 sim.pendulum_values(run_time, init_ang_pos, init_ang_vel, g, L,
                            list_of_times, ang_pos, ang_vel, ang_accel)
 sim.plot_pva(list_of_times, ang_pos, ang_vel, ang_accel, run_time)
-
-
-
-m = input("What run trial would you like to use? ")
-
-x, y, z = sim.read_file(m)
-run_time = (len(x) /10000)
-sim.pendulum_values(run_time, init_ang_pos, init_ang_vel, g, L,
-                           list_of_times, ang_pos, ang_vel, ang_accel)
-pos = sim.tilt(x, y, z)
-
-ang_pos_filt = sim.apply_filter(ang_pos)
-pos_filt = sim.apply_filter(pos)
-
-ang_pos_peaks = sim.find_peaks(ang_pos_filt)
-pos_peaks = sim.find_peaks(pos)
-ang_pos_filt_peaks = sim.find_peaks(ang_pos_filt)
-pos_filt_peaks = sim.find_peaks(pos_filt)
-
-sim.plot_filtered(list_of_times, ang_pos, pos, ang_pos_filt, pos_filt,
-                  ang_pos_peaks, pos_peaks, ang_pos_filt_peaks, pos_filt_peaks)
