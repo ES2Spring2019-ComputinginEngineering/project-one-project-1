fb3d4a522d720893b7ce2d31fe7a8fe17fd744cf
awong15 <Alycia.Wong@tufts.edu>
2019-03-08 11:33:39 -0500

    Data571

--- Step 3.py
+++ Step 3.py
@@ -2,13 +2,15 @@ import microbit
 import random
 
 # collect data from microbit accelerometer and
-m = random.randint(0, 9999)
+m = random.randint(0, 999)
 filename = "Data" + str(m) + ".csv"
 file = open(filename, 'w')
-while True:
-    microbit.sleep(2)
+n = 0
+while n < 505:
+    microbit.sleep(10)
     x = str(microbit.accelerometer.get_x())
     y = str(microbit.accelerometer.get_y())
     z = str(microbit.accelerometer.get_z())
     file.write(x + ',' + y + ',' + z + "\n")
+    n += 1
 file.close()
