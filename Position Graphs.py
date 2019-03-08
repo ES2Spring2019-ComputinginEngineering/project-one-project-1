3e1f507466da8530532640c12ff15183432b0446
Megan Jenney <megan.jenney@tufts.edu>
2019-03-08 11:58:06 -0500

    update

--- Position Graphs.py
+++ Position Graphs.py
@@ -2,7 +2,7 @@ import math
 import Simulation as sim
 
 g = -9.81
-L = 1
+L = .2413
 
 run_time = 5
 init_ang_pos = (math.pi)/6
@@ -15,7 +15,7 @@ ang_pos = [init_ang_pos]
 ang_vel = [init_ang_vel]
 ang_accel = [init_ang_accel]
 
-m = 7862  # input("What file number would you like to use? ")
+m = 571  # input("What file number would you like to use? ")
 
 sim.pendulum_values(run_time, init_ang_pos, init_ang_vel, g, L,
                            list_of_times, ang_pos, ang_vel, ang_accel)
@@ -25,7 +25,9 @@ run_time = (len(x)/1000)
 
 pos = sim.tilt(x, y, z)
 
-ang_pos_filt = sim.apply_filter(ang_pos)
+sim.plot_p(list_of_times, pos, run_time)
+
+"""ang_pos_filt = sim.apply_filter(ang_pos)
 pos_filt = sim.apply_filter(pos)
 
 ang_pos_peaks = sim.find_peaks(ang_pos_filt)
@@ -34,4 +36,4 @@ ang_pos_filt_peaks = sim.find_peaks(ang_pos_filt)
 pos_filt_peaks = sim.filter_peaks(sim.find_peaks(pos_filt))
 
 sim.plot_filtered(list_of_times, ang_pos, pos, ang_pos_filt, pos_filt,
-                  ang_pos_peaks, pos_peaks, ang_pos_filt_peaks, pos_filt_peaks, run_time)
+                  ang_pos_peaks, pos_peaks, ang_pos_filt_peaks, pos_filt_peaks, run_time)"""
