8a8b14b78b2421e8a64388a8dab9a5e9da3eb344
awong15 <Alycia.Wong@tufts.edu>
2019-03-09 11:29:17 -0500

    Trial 5

--- Check.py
+++ Check.py
@@ -1,21 +1,21 @@
-import math
 import Simulation as sim
 
-g = -9.81
-L = 1
+x, y, z = sim.read_file(261)
 
-run_time = 5
-init_ang_pos = (math.pi)/6
-init_ang_vel = 0
-init_ang_accel = (g/L) * math.sin(init_ang_pos)
+pos = sim.tilt(x, y, z)
 
-# lists that will be appended
-list_of_times = [0]
-ang_pos = [init_ang_pos]
-ang_vel = [init_ang_vel]
-ang_accel = [init_ang_accel]
+run_time = len(pos)/100
 
-sim.pendulum_values(run_time, init_ang_pos, init_ang_vel, g, L,
-                           list_of_times, ang_pos, ang_vel, ang_accel)
-t = sim.create_run_time(ang_pos)
-print(t)
+t = sim.create_list_of_times(pos)
+
+sim.plot_p(t, pos, run_time)
+
+x, y, z = sim.read_file(221)
+
+pos = sim.tilt(x, y, z)
+
+run_time = len(pos)/100
+
+t = sim.create_list_of_times(pos)
+
+sim.plot_p(t, pos, run_time)
