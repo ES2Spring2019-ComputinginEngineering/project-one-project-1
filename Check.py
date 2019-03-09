a985cacd29895c461536882411541424c0bf1e0e
awong15 <Alycia.Wong@tufts.edu>
2019-03-09 11:51:43 -0500

    154 = TRIAL 1

--- Check.py
+++ Check.py
@@ -1,6 +1,16 @@
 import Simulation as sim
 
-x, y, z = sim.read_file(809)
+x, y, z = sim.read_file(154)
+
+pos = sim.tilt(x, y, z)
+
+run_time = len(pos)/100
+
+t = sim.create_list_of_times(pos)
+
+sim.plot_p(t, pos, run_time)
+
+x, y, z = sim.read_file()
 
 pos = sim.tilt(x, y, z)
 
